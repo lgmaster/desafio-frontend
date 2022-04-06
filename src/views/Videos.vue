bc6bdb081fa83a356337b1b97d2a44e4577cef82
Luiz Gustavo <lgdeveloper@outlook.com>
2022-04-06 02:47:08 -0300

    feat: created Videos view

---
+++ src/views/Videos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="home" class="container-fluid mt-3">
+    <div class="row mt-3">
+      <div class="col">
+        <MoreVideos />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MoreVideos from "@/components/MoreVideos/index.vue";
+
+export default {
+  name: "Videos",
+  components: {
+    MoreVideos,
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
