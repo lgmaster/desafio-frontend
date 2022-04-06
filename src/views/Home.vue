dba90dd20b4c82c6769f4f2c7f771af9f8e21332
Luiz Gustavo <lgdeveloper@outlook.com>
2022-04-06 02:55:45 -0300

    feat: add to get videos in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="home" class="container-fluid mt-3">
     <div class="row">
-      <div class="col-9">
+      <div class="col-12">
         <Featured />
       </div>
-      <div class="col-3">
+      <!-- <div class="col-3">
         <SquarePopUp />
-      </div>
+      </div> -->
     </div>
     <div class="row mt-3">
       <div class="col">
@@ -17,23 +17,27 @@
 </template>
 
 <script>
-
+import { mapActions } from "vuex";
 import Featured from "@/components/Featured/index.vue";
-import SquarePopUp from "@/components/SquarePopUp/index.vue";
+// import SquarePopUp from "@/components/SquarePopUp/index.vue";
 import MoreVideos from "@/components/MoreVideos/index.vue";
 
 export default {
   name: "Home",
   components: {
     Featured,
-    SquarePopUp,
+    // SquarePopUp,
     MoreVideos,
   },
   data() {
     return {
     };
   },
-  computed: {
+  methods: {
+    ...mapActions("videos", ["listFeatured"]),
+  },
+  async mounted() {
+    await this.listFeatured({ chart: "mostPopular", myRating: null, maxResults: 8 });
   },
 };
 </script>
