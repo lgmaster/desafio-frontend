baed510395147c7adef4e53a36ca4aada5e1f230
Luiz Gustavo <lgdeveloper@outlook.com>
2022-04-06 03:00:12 -0300

    feat: add new routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import SearchVideos from "../views/SearchVideos.vue";
+import Videos from "../views/Videos.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +11,16 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/search",
+    name: "SearchVideos",
+    component: SearchVideos,
+  },
+  {
+    path: "/videos",
+    name: "Videos",
+    component: Videos,
   },
 ];
 
