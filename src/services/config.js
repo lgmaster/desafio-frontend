6cdecf40319c9452433ef36da6c12a06618b63a3
Luiz Gustavo <lgdeveloper@outlook.com>
2022-04-06 00:09:05 -0300

    feat: configure service params

---
+++ src/services/config.js
@@ -0,0 +1,6 @@
+export default {
+  apiKey: "AIzaSyCqrFVqqygg4-tCI4JA1Rp8BPi_CxjW_q8",
+  clientId: "73169226078-7c7e746k90jlidmntvi5ihu226m54dj8.apps.googleusercontent.com",
+  scope: "https://www.googleapis.com/auth/youtube",
+  refreshToken: true,
+};
