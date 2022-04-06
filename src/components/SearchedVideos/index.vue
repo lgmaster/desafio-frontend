46a1ee7abea1178b5150abd04a0ff4cf75b35225
Luiz Gustavo <lgdeveloper@outlook.com>
2022-04-06 02:52:03 -0300

    feat: add SearchVideos component

---
+++ src/components/SearchedVideos/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-body">
+        <h5>Searched Videos</h5>
+        <div v-if="getSearchedVideos"
+        class="row p-3 d-flex
+        align-items-center justify-content-center">
+          <SmallVideo v-for="video in getSearchedVideos" :key="video.id.videoId" :video="video" />
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import SmallVideo from "@/components/SmallVideo/index.vue";
+
+export default {
+  name: "SearchedVideos",
+  components: {
+    SmallVideo,
+  },
+  computed: {
+    ...mapGetters("videos", [
+      "getSearchedVideos",
+    ]),
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/SearchVideos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="home" class="container-fluid mt-3">
+    <div class="row mt-3">
+      <div class="col">
+        <SearchedVideos />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchedVideos from "@/components/SearchedVideos/index.vue";
+
+export default {
+  name: "SearchVideos",
+  components: {
+    SearchedVideos,
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
