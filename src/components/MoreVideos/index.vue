ee219e6a68f174cd4c0f37cf20e5fb095f033f57
Luiz Gustavo <lgdeveloper@outlook.com>
2022-04-06 02:56:04 -0300

    feat: change de limit of videos

--- src/components/MoreVideos/index.vue
+++ src/components/MoreVideos/index.vue
@@ -1,26 +1,33 @@
 <template>
-  <div class="card">
+  <div class="card mb-4">
     <div class="card-body">
-      <h5>More Videos</h5>
-        <div class="row mt-3">
-          <SmallVideo />
-          <SmallVideo />
-          <SmallVideo />
-          <SmallVideo />
-          <SmallVideo />
-          <SmallVideo />
+        <h5>More Videos</h5>
+        <div v-if="getMoreVideos" class="row p-3 d-flex align-items-center justify-content-center">
+          <SmallVideo v-for="video in getMoreVideos" :key="video.id.videoId" :video="video" />
         </div>
       </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
 import SmallVideo from "@/components/SmallVideo/index.vue";
 
 export default {
   name: "MoreVideos",
   components: {
     SmallVideo,
+  },
+  computed: {
+    ...mapGetters("videos", [
+      "getMoreVideos",
+    ]),
+  },
+  methods: {
+    ...mapActions("videos", ["moreVideos"]),
+  },
+  async mounted() {
+    await this.moreVideos({ chart: "mostPopular", myRating: null, maxResults: 40 });
   },
 };
 </script>
